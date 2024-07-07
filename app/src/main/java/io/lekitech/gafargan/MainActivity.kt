b8274100c60f81e0b09e7600fcd13f0d79995d55
Kamran Tadzjibov <ktadzjibov@gmail.com>
2024-07-07 12:29:05 +0200

    allow only baseurl, handle back button press

--- app/src/main/java/io/lekitech/gafargan/MainActivity.kt
+++ app/src/main/java/io/lekitech/gafargan/MainActivity.kt
@@ -8,8 +8,10 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 
 class MainActivity : ComponentActivity() {
+    val baseUrl = "https://gafalag.com";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val webView = WebView(this).apply {
@@ -21,13 +23,31 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Log.e("WebView", "Error: ${error?.description}")
                 }
+                override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                    val url = request?.url.toString()
+                    if (url.startsWith(baseUrl)) {
+                        return false // Allow loading the URL
+                    }
+                    return true // Block all other URLs
+                }
             }
             webChromeClient = WebChromeClient()
             settings.javaScriptEnabled = true
             settings.domStorageEnabled = true
             settings.javaScriptCanOpenWindowsAutomatically = true
-            loadUrl("https://gafalag.com")
+            loadUrl(baseUrl)
         }
         setContentView(webView)
+
+        // Handle back button to navigate within the WebView
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    finish() // Exit the app if there's no web history
+                }
+            }
+        })
     }
 }

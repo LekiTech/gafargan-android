61679b5e478a9ebe4cc9d0bff49a794ca9bb11a0
Kamran Tadzjibov <ktadzjibov@gmail.com>
2024-10-15 19:33:29 +0200

    updated app with optional fuzzy search

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.lekitech.gafargan"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
